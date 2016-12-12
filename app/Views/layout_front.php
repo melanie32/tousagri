016c7289c3bb215f524bfbbc7a932c395c2db023
YannMo <yann.mora10@gmail.com>
2016-12-12 10:21:12 +0100

    voila voila

--- app/Views/layout_front.php
+++ app/Views/layout_front.php
@@ -5,7 +5,7 @@
 	<title><?= $this->e($title) ?>Home</title>
 
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/style_front.css') ?>">
 </head>
 <body>
 	<div class="container">
