1828265ba93ece386dde2e8948b0d919c391ec39
YannMo <yann.mora10@gmail.com>
2016-12-12 15:06:56 +0100

    bim bam boum

---
+++ app/Controller/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Controller;
+
+use \W\Controller\Controller;
+
+class AdminController extends Controller
+{
+
+	/**
+	 * Page de connexion du back office
+	 * 
+	 */
+	public function home()
+	{
+		$this->show('admin/admin_home');
+	}
+
+	/**
+	 * Page d'accueil du back office
+	 * 
+	 */
+	public function accueil()
+	{
+		$this->show('admin/admin_accueil');
+	}
+
+}
--- app/Views/layout_back.php
+++ app/Views/layout_back.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
+<head> 
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style_back.css') ?>">
 </head>
 <body>
-	<div class="container">
+	<div id="wrapper" class="container">
 		<header>
-			<h1><?= $this->e($title) ?></h1>
+			
+
 		</header>
 
 		<section>
@@ -20,5 +23,7 @@
 		<footer>
 		</footer>
 	</div>
+	
+
 </body>
 </html>
