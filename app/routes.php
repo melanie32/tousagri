d59b0fc0e59334ee3ba6db58f4144cdcfa95c8fb
YannMo <yann.mora10@gmail.com>
2016-12-12 15:24:08 +0100

    okok

--- app/routes.php
+++ app/routes.php
@@ -2,14 +2,10 @@
 	// et là ca prend encompte
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'default_home'],
-<<<<<<< HEAD
 		['GET', '/questions', 'Questions#home', 'questions_home'],
-=======
-
-
 
 		// ROUTES POUR LE BACK avec le controller ADMIN
 		['GET|POST', '/admin', 'Admin#home', 'admin_home'],
 		['GET', '/admin/accueil', 'Admin#accueil', 'admin_accueil'],
->>>>>>> origin/master
+
 	);
