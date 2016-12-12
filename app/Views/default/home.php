fd6bb7952531dbb512efc2ccd45bba89bb689570
AnthonyDoux <anthony.doux@live.fr>
2016-12-12 15:06:08 +0100

    paf chocapic

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -1,9 +1,38 @@
-<?php $this->layout('layout', ['title' => 'Accueil']) ?>
+<?php $this->layout('layout_front', ['title' => '']) ?>
 
 <?php $this->start('main_content') ?>
-	<h1>Vous les agriculteurs vous êtes tous les mêmes!</h1>
-	
-	<div class="row">
-		<div class=""><button></button></div>
+
+<!-- Titre -->
+	<div class="col-md-12 headLine">
+		<h1 id="text" class="hLine">Vous les agriculteurs, vous êtes tous les mêmes!</h1>
 	</div>
+
+<!-- Boutton -->
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 startBut">
+						<button id="reset" data-bound="true" type="button" class="btn btn-default start">Commencer</button>
+					</div>
+				</div>
+			</div>
+
+<!-- Footer -->
+	<footer class="bottNav">
+
+	
+			<div>
+				<div class="col-md-12 col-md-offset-6">
+					<ul class="navList">
+						<a href="#">A propos</a>
+						<a href="#">Partenaires</a>
+						<a href="#">Partager</a>
+						<a href="#">Crédits</a>
+						<a href="#">Mentions Légales</a>
+					</ul>
+				</div>
+			</div>
+	
+
+	</footer>
+
 <?php $this->stop('main_content') ?>
--- app/Views/layout_front.php
+++ app/Views/layout_front.php
@@ -6,6 +6,7 @@
 
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style_front.css') ?>">
+	<script type="text/javascript" href="<?= $this->assetUrl('js/script.js') ?>"></script>
 </head>
 <body>
 	<div class="container">
