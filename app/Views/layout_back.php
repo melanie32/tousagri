8a2f57c49a1469afe52922ae48da716674b745f8
melanie32 <dupuymelanie87@yahoo.fr>
2016-12-12 10:21:04 +0100

    fsd

--- app/Views/layout_back.php
+++ app/Views/layout_back.php
@@ -5,7 +5,7 @@
 	<title><?= $this->e($title) ?></title>
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/style_back.css') ?>">
 </head>
 <body>
 	<div class="container">
