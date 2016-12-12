ceaaa1ccf5f43fefe5cb49106d5aef6a1ab14a94
melanie32 <dupuymelanie87@yahoo.fr>
2016-12-12 15:05:29 +0100

    baaaam commit

---
+++ app/Views/admin/admin_accueil.php
@@ -0,0 +1,10 @@
+<?php $this->layout('layout_back', ['title' => 'Accueil']) ?>
+
+<?php $this->start('main_content') ?>
+
+	<p class="text-left text-hello">Bonjour, nom utilisateur en dynamique</p>
+
+	
+
+	
+<?php $this->stop('main_content') ?>
--- app/routes.php
+++ app/routes.php
@@ -2,4 +2,10 @@
 	// et là ca prend encompte
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'default_home'],
+
+
+
+		// ROUTES POUR LE BACK avec le controller ADMIN
+		['GET|POST', '/admin', 'Admin#home', 'admin_home'],
+		['GET', '/admin/accueil', 'Admin#accueil', 'admin_accueil'],
 	);
