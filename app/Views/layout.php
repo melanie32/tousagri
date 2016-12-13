2f092369cb51b24d411676582b0078dfd8b5694a
YannMo <yann.mora10@gmail.com>
2016-12-13 18:46:11 +0100

    MAJ du soir

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -41,8 +41,8 @@
 
 	</footer>
 
-	</div>
 <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?= $this->assetUrl('js/scriptQuestion.js') ?>"></script>
 <script type="text/javascript" src="<?= $this->assetUrl('js/script.js') ?>"></script>
 
