0d098ce25637dda41768762f36182272159e38d2
YannMo <yann.mora10@gmail.com>
2016-12-13 16:26:49 +0100

    MAJ

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -5,6 +5,9 @@
 	<title><?= $this->e($title) ?></title>
 
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+
 	
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style_front.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style_frontQuestion.css') ?>">
@@ -18,17 +21,14 @@
 			<?= $this->section('main_content') ?>
 		</section>
 
-<<<<<<< HEAD:app/Views/layout_front.php
-<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-<script type="text/javascript" src="<?= $this->assetUrl('js/scriptQuestion.js') ?>"></script>
-
-=======
 		<footer>
 		</footer>
 	</div>
 
 
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="<?= $this->assetUrl('js/scriptQuestion.js') ?>"></script>
 	<script type="text/javascript" src="<?= $this->assetUrl('js/script.js') ?>"></script>
->>>>>>> origin/master:app/Views/layout.php
+	
 </body>
 </html>
