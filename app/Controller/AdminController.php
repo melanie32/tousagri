3b79870ee07169233a4a28bd247fd9fdf0f59b23
melanie32 <dupuymelanie87@yahoo.fr>
2016-12-14 13:28:39 +0100

    la function qui va bien ou mal

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -5,6 +5,10 @@ namespace Controller;
 use \W\Controller\Controller;
 use \Model\CategoriesModel;
 
+use \Respect\Validation\Validator as v; 
+
+use \Intervention\Image\ImageManagerStatic as Image;
+
 class AdminController extends Controller
 {
 
@@ -43,8 +47,250 @@ class AdminController extends Controller
 
 	public function addCategories()
 	{
-		$this->show('admin/admin_add_categories');
-	}
+		$post = [];
+		$errors = [];
+		$sucess = false;
+
+		// préparation des dossiers pour l'upload de picto
+		$folderUploadPicto = getApp()->getConfig('upload_dir_picto'); //permet d'indiquer via le fichier config le dossier destinataire de l'upload de picto
+
+		$fullFolderUploadPicto = $_SERVER['DOCUMENT_ROOT'].$_SERVER['W_BASE'].'/assets/img/'.$folderUploadPicto;
+
+
+
+
+		// préparation des dossiers pour l'upload de l'illus
+		$folderUploadIllus = getApp()->getConfig('upload_dir_illus'); //permet d'indiquer via le fichier config le dossier destinataire de l'upload de l'illus
+
+		$fullFolderUploadIllus = $_SERVER['DOCUMENT_ROOT'].$_SERVER['W_BASE'].'/assets/img/'.$folderUploadIllus;
+
+
+
+
+		// préparation des dossiers pour l'upload des images illustrant la réponse à question
+		$folderUploadPicture = getApp()->getConfig('upload_dir_imgreply'); //permet d'indiquer via le fichier config le dossier destinataire de l'upload de l'image illustrant les réponses des questions
+
+		$fullFolderUploadPicture = $_SERVER['DOCUMENT_ROOT'].$_SERVER['W_BASE'].'/assets/img/'.$folderUploadPicture;
+		
+
+		if(!empty($_POST)) {
+
+			$post = array_map('trim', array_map('strip_tags', $_POST));
+
+			if(!v::notEmpty()->length(2, 50)->validate($post['title'])){
+					$errors[] = 'Le titre de la catégorie doit comporter entre 2 et 50 caractères';
+			}
+
+			// validation du mime type des images
+
+			// validation du pictogramme
+			if(!v::image()->validate($_FILES['pictogram']['tmp_name'])){
+					$errors[] = 'Le fichier envoyé pour le pictogramme n\'est pas une image valide';
+			}
+			// Valide la taille du pictogramme
+			if(!v::size(null, '2MB')->validate($_FILES['pictogram']['tmp_name'])){
+				$errors[] = 'La taille de votre pictogramme doit être inférieur à 2MB';
+			}
+				// Vérifie que le pictogramme a bien été uploadé
+			if(!v::uploaded()->validate($_FILES['pictogram']['tmp_name'])){
+				$errors[] = 'Une erreur est survenue lors de l\'upload du pictogramme';
+			}
+
+
+
+			// validation de l'illustration
+			if(!v::image()->validate($_FILES['illustration']['tmp_name'])){
+					$errors[] = 'Le fichier envoyé pour le pictogramme n\'est pas une image valide';
+			}
+			// Valide la taille de l'illustration
+			if(!v::size(null, '2MB')->validate($_FILES['illustration']['tmp_name'])){
+				$errors[] = 'La taille de votre pictogramme doit être inférieur à 2MB';
+			}
+				// Vérifie que l'illustration a bien été uploadé
+			if(!v::uploaded()->validate($_FILES['illustration']['tmp_name'])){
+				$errors[] = 'Une erreur est survenue lors de l\'upload du pictogramme';
+			}
+
+
+
+			if(!v::notEmpty()->length(5, 150)->validate($post['question'])){
+					$errors[] = 'La question doit comporter entre 5 et 150 caractères';
+			}
+
+			if(!v::notEmpty()->length(5, 10000)->validate($post['explanation'])){
+					$errors[] = 'La réponse doit comporter entre 5 et 10000 caractères';
+			}
+
+
+
+			// validation de l'image contenue dans le texte de réponse
+			if(!v::image()->validate($_FILES['picture']['tmp_name'])){
+					$errors[] = 'Le fichier envoyé pour l\'image illustant la question n\'est pas une image valide';
+			}
+			// Valide la taille de l'image contenue dans le texte de réponse
+			if(!v::size(null, '2MB')->validate($_FILES['picture']['tmp_name'])){
+				$errors[] = 'La taille de votre image illustant la question doit être inférieur à 2MB';
+			}
+				// Vérifie que l'image contenue dans le texte de réponse a bien été uploadé
+			if(!v::uploaded()->validate($_FILES['picture']['tmp_name'])){
+				$errors[] = 'Une erreur est survenue lors de l\'upload de l\'image illustant la question';
+			}
+
+			if(!v::url()->validate($post['video']) {
+				$errors[] = 'l\'URL de la vidéo Youtube n\'est pas valide';
+			}
+
+			if(count($errors) === 0) {
+
+				// dossier des pictos => /public/assets/img/pictos
+				// créer le dossier pictos si inexistant
+
+				if(!is_dir($fullFolderUploadPicto)){
+					mkdir($fullFolderUploadPicto, 0755);
+				}
+
+				$picto = Image::make($_FILES['pictogram']['tmp_name']);
+
+				// On définit l'extension de l'image en fonction de son mimeType
+				switch($picto->mime()){
+						case 'image/jpg':
+						case 'image/jpeg':
+							$extension = '.jpg';
+						break;
+						case 'image/png':
+							$extension = '.png';
+						break;
+						case 'image/gif':
+							$extension = '.gif';
+						break;
+
+				}
+
+				// Le nom du picto + son extension
+				$pictoName = uniqid('art_').$extension;
+
+				// On sauvegarde l'image 
+				$picto->save($fullFolderUploadPicto.$pictoName);
+
+
+
+				// dossier des illustrations => /public/assets/img/illustrations
+				// créer le dossier illustrations si inexistant
+
+				if(!is_dir($fullFolderUploadIllus)){
+					mkdir($fullFolderUploadIllus, 0755);
+				}
+
+				$illus = Image::make($_FILES['illustration']['tmp_name']);
+
+				// On définit l'extension de l'image en fonction de son mimeType
+				switch($illus->mime()){
+						case 'image/jpg':
+						case 'image/jpeg':
+							$extension = '.jpg';
+						break;
+						case 'image/png':
+							$extension = '.png';
+						break;
+						case 'image/gif':
+							$extension = '.gif';
+						break;
+
+				}
+
+				// Le nom de l'illus + son extension
+				$illusName = uniqid('art_').$extension;
+
+				// On sauvegarde l'illus 
+				$illus->save($fullFolderUploadIllus.$illusName);
+
+
+
+				// dossier des image contenue dans la réponse => /public/assets/img/imgreply
+				// créer le dossier imgreply si inexistant
+
+				if(!is_dir($fullFolderUploadPicture)){
+					mkdir($fullFolderUploadPicture, 0755);
+				}
+
+				$picture = Image::make($_FILES['picture']['tmp_name']);
+
+				// On définit l'extension de l'image en fonction de son mimeType
+				switch($picture->mime()){
+						case 'image/jpg':
+						case 'image/jpeg':
+							$extension = '.jpg';
+						break;
+						case 'image/png':
+							$extension = '.png';
+						break;
+						case 'image/gif':
+							$extension = '.gif';
+						break;
+
+				}
+
+				// Le nom du picto + son extension
+				$pictureName = uniqid('art_').$extension;
+
+				// On sauvegarde l'image 
+				$picture->save($fullFolderUploadPicture.$pictureName);
+
+
+
+
+				// on instancie le modèle pour communiquer avec la bdd
+				// le modèle categories pour faire le lien avec la table categories
+				// insertion du titre, du picto et de l'illus
+				$categoriesModel = new CategoriesModel();
+
+				$insertCategories = $categoriesModel->insert([
+					'title' => $post['title'],
+					'pictogram' => $pictoName,
+					'illustration' => $illusName,
+				]);
+
+				if($insertCategories) {
+
+					// si l'insertion dans la table categories a été effectué alors on insère dans la table questions le reste des données du formulaire
+
+					// on instancie le modèle pour communiquer avec la bdd
+					// le modèle questions pour faire le lien avec la table questions
+					// insertion de la question, explanation, video et picture
+
+					$questionsModel = new QuestionsModel();
+
+					$insertQuestions = $questionsModel->insert([
+						'id_category' => $insertCategories,
+						'question' => $post['question'],
+						'explanation' => $post['explanation'],
+						'picture' => $pictureName,
+						'video' => $post['video'],
+					]);
+
+					if($insertQuestions) {
+						$sucess = true;						
+					}
+					else {
+						$errors[] = 'Erreur lors de l\'ajout de la question en base de données';
+					}
+				}
+				else {
+					$errors[] = 'Erreur lors de l\'ajout total en base de données';
+				}
+				
+			} //end of count errors
+
+		} // end of if $_POST empty
+
+		$params = [
+			'errors' => $errors,
+			'sucess' => $sucess,
+		];
+
+		$this->show('admin/admin_add_categories', $params);
+
+	} // end function addcategories
 
 	/**
 	 * Page  modifier catégories du back office
--- app/Views/admin/admin_add_categories.php
+++ app/Views/admin/admin_add_categories.php
@@ -1,13 +1,13 @@
 <?php $this->layout('layout_back', ['title' => 'Ajouter une catégorie']) ?>
 
-<?php $this->start('main_content') ?>
+<?php $this->start('main_content') ?> 
  
 	<div class="text-center">
 		<p class="text-connect">Ajouter une catégorie</p>
 	</div>
 	<br>
 
-	<form class="form-horizontal" method="post">
+	<form class="form-horizontal" method="post" enctype="multipart/form-data">
 
 		<!-- Text input-->
 		<div class="form-group">
@@ -33,9 +33,13 @@
 			</div>
 		</div>
 
+		<br>
+		
+		<br>
+
 		<!-- Text input-->
 		<div class="form-group">
-			<label class="col-md-4 control-label" for="question">Question $i</label>  
+			<label class="col-md-4 control-label" for="question">Question $i</label>
 			<div class="col-md-4">
 				<input id="question" name="question" placeholder="" class="form-control input-md" type="text">   
 			</div>
@@ -48,6 +52,23 @@
 				<textarea class="form-control" id="explanation" name="explanation"></textarea>
 			</div>
 		</div>
+
+		<!-- File Button --> 
+		<div class="form-group">
+			<label class="col-md-4 control-label" for="picture">Photo illustrant la réponse</label>
+			<div class="col-md-4">
+				<input id="picture" name="picture" class="input-file" type="file">
+			</div>
+		</div>
+
+		<!-- Text input-->
+		<div class="form-group">
+			<label class="col-md-4 control-label" for="video">Lien vers une vidéo (youtube uniquement)</label>
+			<div class="col-md-4">
+				<input id="video" name="video" placeholder="" class="form-control input-md" type="text">   
+			</div>
+		</div>
+
 
 		<!-- pour ajouter une question et réponse -->
 		<div class="form-group">
