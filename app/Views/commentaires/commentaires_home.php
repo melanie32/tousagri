212954cd40a66d47731772f47ca843bda563249f
AnthonyDoux <anthony.doux@live.fr>
2016-12-15 18:57:02 +0100

    fg

--- app/Views/commentaires/commentaires_home.php
+++ app/Views/commentaires/commentaires_home.php
@@ -1,5 +1,15 @@
 <?php $this->layout('layout', ['title' => 'Commentaires']) ?>
 
+<?php $this->start('css') ?>
+
+<style>
+.wrapper_com{
+	background-image: url("<?=$this->assetUrl('img/champs.jpg')?>");
+}
+
+</style>
+<?php $this->stop('css') ?>
+
 <?php $this->start('main_content') ?>
 
 <div class="commentaire">
@@ -12,7 +22,7 @@
 	<div class="contenu_commentaires">
 		<h3 class="title_nbr_commentaires">$i commentaires</h3>
 
-		<ul class="commentlist">
+		<!-- <ul class="commentlist">
 			<li class="comment even thread-even depth-1 clearfix dnt_bordercolor_current_ep " id=".....">
 
 				<div class="comment-block" id="....">
@@ -38,17 +48,42 @@
 
 				</div>
 			</li>
-		</ul>
+		</ul> -->
+
+		<form class="form-horizontal" method="POST">
 
 
+
+
+
+<!-- username input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="username">Pseudo</label>  
+  <div class="col-md-4">
+  <input id="username" name="username" type="text" placeholder="Pseudo..." class="form-control input-md"> 
+  </div>
+</div>
+
+<!--content input -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="coment">Commentaire</label>
+  <div class="col-md-4">                     
+    <textarea class="form-control" id="textarea" name="coment">Votre commentaire...</textarea>
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="submit"></label>
+  <div class="col-md-4">
+    <button id="submit" name="submit" class="btn btn-primary">Envoyer</button>
+  </div>
+</div>
+
+
+</form>
+
+ <!---//////////////////// ATTENTION AFFICHAGE COMENTS TEST /////////////////////-->
 
 <?php $this->stop('main_content') ?>
 
-<?php $this->start('css') ?>
-<style>
-.wrapper_com{
-	background-image: url("<?=$this->assetUrl('img/champs.jpg')?>");
-}
-
-</style>
-<?php $this->stop('css') ?>
