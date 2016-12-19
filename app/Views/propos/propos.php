d6e4d2ea1594c5202e5823bd79d64f0aebd5f43e
YannMo <yann.mora10@gmail.com>
2016-12-19 13:37:19 +0100

    fthdf

--- app/Views/propos/propos.php
+++ app/Views/propos/propos.php
@@ -5,10 +5,16 @@
 <div id="wrapper_Mentions">
 
 		<div class="title_conteneur_L">
-			<h1>- Mention Légale -</h1>
+			<h1>- A Propos -</h1>
 		</div>
 
 	<div class="bloc_nav_opacity"></div> 
+</div>
+<div id="bloc_mentions">
+<h2>Tout commence au 66 Rue de l'abbée de l'épée...</h2>
+	<p>A-Propos Langues® est un organisme de formation en langues, certifié NF214, et dispose d’un service de traduction. Nous sommes spécialisés dans les services linguistiques sur mesure et de qualité, pour les entreprises et les administrations.<br>
+	Depuis 1997, A-Propos Langues® s'est forgé une solide réputation basée sur son professionnalisme et son esprit d'innovation. Nos solutions de formation, de traduction et de séjours linguistiques en 35 langues regroupent tous les aspects linguistiques, culturels et technologiques afin d’apporter des réponses concrètes.<br>
+	Parce que le monde est en perpétuelle mutation, A-Propos Langues® investit continuellement dans la recherche pédagogique et technologique.</p>
 </div>
 
 
