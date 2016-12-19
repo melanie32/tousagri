f558602cf9cf0a3e707f6b9d31beb14384cc4350
melanie32 <dupuymelanie87@yahoo.fr>
2016-12-19 13:51:02 +0100

    oui

--- public/assets/js/scriptback.js
+++ public/assets/js/scriptback.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
 	$('#addinput').click(function() {
 		var newinput = '<td><input class="form-control input-md" type="text" name="question[]" required></td>';
 		newinput += '<td><textarea rows="7" cols="50" name="explanation[]" class="form-control" id="explanation" required></textarea></td>';
-		newinput += '<td><label>Ajouter une image:</label><br><input type="file" name="picture[]" required><br>';
-		newinput += '<label>Ajouter une URL vidéo:</label><br><input type="text" name="video[]" required></td>';
+		newinput += '<td><label>Ajouter une image:</label><br><input type="file" name="picture[]"><br>';
+		newinput += '<label>Ajouter une URL vidéo:</label><br><input type="text" name="video[]"></td>';
 		
 		$('#new-input').append('<tr id="newinputQ">'+newinput+'</tr>'); 	
 	});
