ce0c230d4f62ea7f9392111b2385faffcf100e2c
YannMo <yann.mora10@gmail.com>
2016-12-22 12:24:34 +0100

    Maj

--- app/Views/w_errors/404.php
+++ app/Views/w_errors/404.php
@@ -1,5 +1,8 @@
 <?php $this->layout('layout', ['title' => 'Perdu ?']) ?>
 
 <?php $this->start('main_content'); ?>
-<h1>404. Perdu ?</h1>
+<h1 class="text-center" style="margin-top:3em; color:red;">T'as tout cassé Serge -_- !!!!!!</h1>
+<h2 class="text-center" style="font-size:3em; margin-top:5em">La page est partie ... cours lui après, on sait jamais ... </h2>
+<p class="text-center" style="font-size:1.5em;">Conseil de TousAgri.fr</p>
+
 <?php $this->stop('main_content'); ?>
